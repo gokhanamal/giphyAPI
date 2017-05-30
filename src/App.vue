c71ad91d5bf27a3ae0d2ffb0f049e9a85aaca354
Gokhan Namal <gokhanamal@gmail.com>
2017-05-30 13:42:03 +0300

    deploy edildi

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    
+   <search @SearchRequested="handleSearch"></search>
+   <p v-if="isLoadig">Loading..</p>
+   <preview :gifs=gifs></preview>
+  </div>
+</template>
+
+<script>
+import Search from './components/Search.vue'
+import Preview from './components/Preview.vue'
+export default {
+  name: 'app',
+  components:{Search, Preview},
+  data(){
+    return {
+      isLoadig:true,
+      gifs:[]
+    }
+  },
+  methods:{
+    handleSearch(query){
+      this.gifs=[];
+      this.isLoadig=true;
+      fetch(`http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC `)
+      .then((res)=> {return res.json()})
+      .then((res)=> {
+      this.gifs=res.data;
+      this.isLoadig=false;})
+
+    }
+  },
+  created(){
+    fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+    .then((res)=> {return res.json()})
+    .then((res)=> {
+      this.gifs=res.data;
+      this.isLoadig=false;})
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+}
+
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="search">
+        <input type="text" v-model="query" @keypress.enter="handleSearch"/>
+        <button @click="handleSearch">Search</button>     
+    </div>
+</template>
+
+<script>
+    export default {
+        data (){
+            return {
+                query:''
+            }
+        },
+        methods:{
+            handleSearch() {
+                this.$emit('SearchRequested',this.query);
+            }
+        }
+    }
+</script>
+
+<style>
+  .search input {
+      padding: 10px 20px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      width: 400px;
+  }
+  .search button {
+        width: 75px;
+        height: 38px;
+        border-radius: 5px;
+        border:1px solid #ccc;
+        font-size: 15px;
+        color: white;
+        background-color: #2866AB;
+
+  }
+
+  .search button:hover {
+      filter: brightness(120%);
+  }
+  .search {
+      text-align: center;
+  }
+</style>
